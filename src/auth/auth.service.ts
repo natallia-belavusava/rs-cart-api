ff937df1c47655754e7a7614e500133ea05faaab
Natallia_Belavusava <Natallia_Belavusava@epam.com>
2023-04-07 14:39:51 +0200

    integrate with user service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -11,14 +11,14 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  validateUser(name: string, password: string): any {
-    const user = this.usersService.findOne(name);
+  async validateUser(name: string, password: string, email: string): Promise<any> {
+    const user = await this.usersService.findOne(name);
 
     if (user) {
       return user;
     }
 
-    return this.usersService.createOne({ name, password });
+    return await this.usersService.createOne({name, password, email});
   }
 
   login(user: User, type) {
--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -3,34 +3,38 @@ import { Injectable } from '@nestjs/common';
 import { v4 } from 'uuid';
 
 import { User } from '../models';
-import { getDBClient } from '../../lib/db';
+import {getDBClient} from "../../lib/db";
 
 @Injectable()
 export class UsersService {
   private readonly users: Record<string, User>;
 
-  async findOne(userId: string): Promise<User> {
+
+  async findOne(name: string): Promise<User> {
     try {
       const client = await getDBClient();
-      return await client.query('SELECT * FROM users WHERE user_id = $1', [
-        userId,
-      ]);
+      return await client.query(
+          'SELECT * FROM users WHERE name = $1',
+          [name],
+      );
     } catch (err) {
-      console.log(err);
+      console.log(err)
     }
   }
 
-  async createOne({ name, password, email }: User): Promise<User> {
+  async createOne({name, password, email}: User): Promise<User> {
     try {
       const client = await getDBClient();
 
       const id = v4(v4());
       const user = await client.query(
-        'insert into cart_items (id, name, password, email) values ($1, $2, $3, $4) RETURNING *',
-        [id, name, password, email],
+          'insert into users (id, name, password, email) values ($1, $2, $3, $4) RETURNING *',
+          [id, name, password, email],
       );
-      client.end();
-      return user;
-    } catch (e) {}
+       client.end();
+       return user;
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
